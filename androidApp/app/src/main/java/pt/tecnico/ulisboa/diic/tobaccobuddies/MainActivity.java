7dd49efbd280d426fa4f97d446bdb09c9e878bbd
Alexandra Maroco <alexandra.maroco@tecnico.ulisboa.pt>
2019-11-22 13:29:22 +0000

    app starting point

---
+++ androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/MainActivity.java
@@ -0,0 +1,13 @@
+package pt.tecnico.ulisboa.diic.tobaccobuddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
