717408d706e99aa9860352a7200677e1276fb714
Diogo Freitas <diogo.maria.freitas@gmail.com>
2019-11-25 19:00:46 +0000

    Arduino board change, Bluetooth communication

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,12 +1,23 @@
 #include <math.h>
+#include <BluetoothSerial.h>
 
 /* --- GLOBAL VARIABLES --- */
 
-const int INCR_BUTTON = 2;
-const int DECR_BUTTON = 3;
-const int R_PIN = 9;
-const int G_PIN = 10;
-const int B_PIN = 11;
+const int INCR_BUTTON = 23;
+const int DECR_BUTTON = 22;
+const uint8_t R_PIN = 18;
+const uint8_t G_PIN = 19;
+const uint8_t B_PIN = 21;
+const uint8_t R_LED = 1;
+const uint8_t G_LED = 2;
+const uint8_t B_LED = 3;
+
+const char MESSAGE_START_CHAR = '<';
+const char MESSAGE_ENDED_CHAR = '>';
+
+BluetoothSerial esp32BT;
+
+char messageReceived[100];
 
 /* --- INPUT DETECTION VARIABLES --- */
 
@@ -26,65 +37,32 @@ int decrTimeEndPressed = 0;               // Time the button was released
 int decrTimeHold = 0;                     // How long the button was held
 int decrTimeReleased = 0;                 // How long the button released
 
+int timeLedChange = 0;
+bool flagLEDBlink_awtConn = false;
+
 /* --- FUNCTIONALITY VARIABLES --- */
 
 int cigarettesSmoked = 0;
 int limit = 10;
 
-/* --- AUXILIARY METHODS --- */
-
-void setColor (unsigned char red, unsigned char green, unsigned char blue) {
-  analogWrite(R_PIN, red);   
-  analogWrite(G_PIN, green); 
-  analogWrite(B_PIN, blue); 
-}
-
-void ledBlink(int nTimes, float nTimeAppart, unsigned char red, unsigned char green, unsigned char blue) {
-  int prevColor[] = {analogRead(R_PIN), analogRead(G_PIN), analogRead(B_PIN)};
-  int millis = nTimeAppart * 1000;
-  for(int i = 0; i <= nTimes - 1; i++) {
-    setColor(red, green, blue); delay(millis); setColor(0, 0, 0); delay(millis);
-  }
-  setColor(prevColor[0], prevColor[1], prevColor[2]);
-}
-
-/* --- FUNCTIONALITY METHODS --- */
-
-void incrementCigsSmoked() {
-  cigarettesSmoked++;
-  Serial.print("Number of Cigarettes Smoked: "); Serial.println(cigarettesSmoked);
-  if (cigarettesSmoked <= limit)
-    ledBlink(2, 0.5, 255, 0, 0);
-  else
-    ledBlink(5, 0.25, 255, 0, 0);
-}
-
-void decrementCigsSmoked() {
-  if (cigarettesSmoked > 0) {
-    cigarettesSmoked--;
-    Serial.print("Number of Cigarettes Smoked: "); Serial.println(cigarettesSmoked);
-    ledBlink(2, 0.5, 0, 255, 0);
-  }
-}
-
-void setColorBasedOnSmoked() {
-  float percentage = (float) cigarettesSmoked / (float) limit;
-  int red = (int) round(255 * percentage);
-  if (red > 255) red = 255;
-  int green = 255 - red;
-  setColor(red, green, 0);
-}
-
 /* --- SETUP & SETUP_AUX METHODS --- */
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  esp32BT.begin("TB Packet");
+  flagLEDBlink_awtConn = true;
+  esp32BT.register_callback(callback);
 
   pinMode(INCR_BUTTON, INPUT);
   pinMode(DECR_BUTTON, INPUT);
-  pinMode(R_PIN, OUTPUT); 
-  pinMode(G_PIN, OUTPUT);
-  pinMode(B_PIN, OUTPUT);
+
+  ledcSetup(R_LED, 5000, 8);
+  ledcSetup(G_LED, 5000, 8);
+  ledcSetup(B_LED, 5000, 8);
+  ledcAttachPin(R_PIN, R_LED);
+  ledcAttachPin(G_PIN, G_LED);
+  ledcAttachPin(B_PIN, B_LED);
 
   setColorBasedOnSmoked();
 }    
@@ -114,6 +92,12 @@ void loop() {
     updateDecrButtonCounter();
   }
 
+  int ledChangeElapsedTime = millis() - timeLedChange;
+  if(flagLEDBlink_awtConn && ledChangeElapsedTime == 5000) {
+    ledBlink(2, 0.15, 0, 0, 255);
+  }
+
+  btReceiveMessage();
   setColorBasedOnSmoked();
   lastIncrButtState = currIncrButtState;
   lastDecrButtState = currDecrButtState;
@@ -157,4 +141,107 @@ void updateDecrButtonCounter() {
     decrTimeHold = millis() - decrTimeStartPressed;                 // Registers for how long the button has been pressed currently
   else
     decrTimeReleased = millis() - decrTimeEndPressed;               // Registers for how long the button has been released currently
+}
+
+/* --- AUXILIARY METHODS --- */
+
+void setColor (uint32_t red, uint32_t green, uint32_t blue) {
+  ledcWrite(R_LED, red);   
+  ledcWrite(G_LED, green); 
+  ledcWrite(B_LED, blue); 
+}
+
+void ledBlink(unsigned char red, unsigned char green, unsigned char blue) {
+  if (ledcRead(R_LED) == 0 && ledcRead(G_LED) == 0 && ledcRead(B_LED) == 0)
+    setColor(red, green, blue);
+  else
+    setColor(0, 0, 0);
+  timeLedChange = millis();
+}
+
+void ledBlink(int nTimes, float nTimeAppart, unsigned char red, unsigned char green, unsigned char blue) {
+  int prevColor[] = {ledcRead(R_LED), ledcRead(G_LED), ledcRead(B_LED)};
+  int mills = nTimeAppart * 1000;
+  for(int i = 0; i <= nTimes - 1; i++) {
+    setColor(red, green, blue); delay(mills); setColor(0, 0, 0); delay(mills);
+  }
+  setColor(prevColor[0], prevColor[1], prevColor[2]);
+  timeLedChange = millis();
+}
+
+void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
+  if(event == ESP_SPP_SRV_OPEN_EVT){
+    Serial.println("Client Connected");
+    esp32BT.print("SMOKED "); esp32BT.println(cigarettesSmoked);
+    flagLEDBlink_awtConn = false;
+  }
+
+  if(event == ESP_SPP_CLOSE_EVT){
+    Serial.println("Client disconnected");
+    flagLEDBlink_awtConn = true;
+    timeLedChange = millis();
+  }
+}
+
+/* --- FUNCTIONALITY METHODS --- */
+
+void incrementCigsSmoked() {
+  cigarettesSmoked++;
+  Serial.print("Number of Cigarettes Smoked: "); Serial.println(cigarettesSmoked);
+  esp32BT.print("SMOKED "); esp32BT.println(cigarettesSmoked);
+  if (cigarettesSmoked <= limit)
+    ledBlink(2, 0.5, 255, 0, 0);
+  else
+    ledBlink(5, 0.25, 255, 0, 0);
+}
+
+void decrementCigsSmoked() {
+  if (cigarettesSmoked > 0) {
+    cigarettesSmoked--;
+    Serial.print("Number of Cigarettes Smoked: "); Serial.println(cigarettesSmoked);
+    esp32BT.print("SMOKED "); esp32BT.println(cigarettesSmoked);
+    ledBlink(2, 0.5, 0, 255, 0);
+  }
+}
+
+void setColorBasedOnSmoked() {
+  float percentage = (float) cigarettesSmoked / (float) limit;
+  int red = analogRead(R_PIN);
+  int green = analogRead(G_PIN);
+  if (percentage <= 0.5) {
+    red = (int) round(255 * (percentage * 2));
+    if (red < 0) red = 0;
+    if (red > 255) red = 255;
+    green = 255;
+  }
+  if (percentage >= 0.5) {
+    green = (int) round(255 * ((1 - percentage) * 2));
+    red = 255;
+    if (green < 0) green = 0;
+    if (green > 255) green = 255;
+  }
+  setColor(red, green, 0);
+}
+
+void btReceiveMessage() {
+  byte i;
+  while (esp32BT.available()) {
+    char c = esp32BT.read();
+    if (c == MESSAGE_START_CHAR) {
+      i = 0;
+      messageReceived[i] = '\0';
+    }
+    else if (c == MESSAGE_ENDED_CHAR) {
+      Serial.print("Received: "); Serial.println(messageReceived);
+      break;
+    }
+    else {
+      if (i < 99) {
+        messageReceived[i] = c;
+        i++;
+        messageReceived[i] = '\0';
+      }
+    }
+  }
+  messageReceived[0] = '\0';
 }
