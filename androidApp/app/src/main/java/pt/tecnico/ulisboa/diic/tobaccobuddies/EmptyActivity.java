98a447adb28d718a58714fcf29dc70f2dd7537ef
Alexandra Maroco <alexandra.maroco@tecnico.ulisboa.pt>
2019-11-28 21:55:16 +0000

    e-puppy & settings icon done

--- androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/EmptyActivity.java
+++ androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/EmptyActivity.java
@@ -9,6 +9,6 @@ public class EmptyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_empty);
+        //setContentView(R.layout.activity_empty);
     }
 }
