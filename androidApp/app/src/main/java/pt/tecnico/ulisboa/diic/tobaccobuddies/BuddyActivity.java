e61b8b7e208653756b7312fa8326a7480f76f771
joanasteodoro <joanasilvateodoro@gmail.com>
2019-12-09 20:28:09 +0000

    all settings working

--- androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/BuddyActivity.java
+++ androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/BuddyActivity.java
@@ -12,11 +12,11 @@ public class BuddyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_buddy);
+        setContentView(R.layout.settings_buddy);
 
 
         // Code for Close (X) button
-        Button closeButton = findViewById(R.id.close_button3);
+        Button closeButton = findViewById(R.id.close_button2);
         closeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
