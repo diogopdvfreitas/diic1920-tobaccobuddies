337992a034101f362b6c3b90758d10ff5f51f64c
joanasteodoro <joanasilvateodoro@gmail.com>
2019-12-09 20:35:12 +0000

    all settings working

--- androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/BuddyActivity.java
+++ androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/BuddyActivity.java
@@ -2,9 +2,11 @@ package pt.tecnico.ulisboa.diic.tobaccobuddies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class BuddyActivity extends AppCompatActivity {
 
@@ -21,6 +23,28 @@ public class BuddyActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+
+        ImageView addBuddyButton = findViewById(R.id.add_buddy);
+        addBuddyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // start an EPuppyActivity
+                Intent intent = new Intent(getApplicationContext(), AddBuddyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        ImageView removeBuddyButton = findViewById(R.id.remove_buddy);
+        removeBuddyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // start an EPuppyActivity
+                Intent intent = new Intent(getApplicationContext(), RemoveBuddyActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/RemoveBuddyActivity.java
@@ -0,0 +1,15 @@
+package pt.tecnico.ulisboa.diic.tobaccobuddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RemoveBuddyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.remove_buddy);
+    }
+}
