0dba362b87f6cac3ec80e49ef0abfa2cb8fae59f
joanasteodoro <joanasilvateodoro@gmail.com>
2019-12-11 18:54:01 +0000

    enter added to add a buddy

--- androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/AddBuddyActivity.java
+++ androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/AddBuddyActivity.java
@@ -2,7 +2,11 @@ package pt.tecnico.ulisboa.diic.tobaccobuddies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class AddBuddyActivity extends AppCompatActivity {
 
@@ -11,5 +15,15 @@ public class AddBuddyActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_buddy);
+
+        ImageButton enterButton = findViewById(R.id.enter);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), BuddyAddedActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/BuddyAddedActivity.java
@@ -0,0 +1,14 @@
+package pt.tecnico.ulisboa.diic.tobaccobuddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuddyAddedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buddy_added);
+    }
+}
