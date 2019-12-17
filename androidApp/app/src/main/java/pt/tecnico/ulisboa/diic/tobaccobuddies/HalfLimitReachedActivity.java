e6d80816c3895a2629fe992990800829beadcd9b
joanasteodoro <joanasilvateodoro@gmail.com>
2019-12-17 16:40:31 +0000

    half limit popup added

---
+++ androidApp/app/src/main/java/pt/tecnico/ulisboa/diic/tobaccobuddies/HalfLimitReachedActivity.java
@@ -0,0 +1,13 @@
+package pt.tecnico.ulisboa.diic.tobaccobuddies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HalfLimitReached extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
